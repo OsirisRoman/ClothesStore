23427891a9e27b4dfc24adefe04d43c378472841
Osiris Román <osirisr1994@gmail.com>
2020-09-25 19:18:55 -0500

    HomePage
    Se añadieron los componentes y la hoja de estilo para la pantalla de bienvenida del Ecommerce

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component'
+
 function App() {
-  return <div>Hello World</div>;
+  return <div>
+    <HomePage></HomePage>
+  </div>;
 }
 
 export default App;
