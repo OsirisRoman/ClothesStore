d5fd1a10383dcec869c8fa2494a262d4c4449dce
Osiris Román <osirisr1994@gmail.com>
2020-09-27 20:57:36 -0500

    Adding Basic Routing
    Se añadieron rutas básicas al proyecto

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+)
+
 function App() {
   return <div>
-    <HomePage></HomePage>
+    <Switch>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/hats' component={HatsPage}></Route>
+    </Switch>
+    {/* <HomePage></HomePage> */}
   </div>;
 }
 
