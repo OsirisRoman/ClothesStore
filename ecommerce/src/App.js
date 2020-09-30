47cf3b8c17c2147dabfbf4af46c7c8dce6e93f95
Osiris Román <osirisr1994@gmail.com>
2020-09-29 20:00:17 -0500

    Hats Navigation Updated
    Se añadieron rutas a los items de nuestro menú usando el componente withRouter

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,46 +1,21 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-/* import HomePage from './pages/homepage/homepage.component' */
+import HomePage from './pages/homepage/homepage.component'
 
-const HomePage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <Link to='/topics'>Topics</Link>
-      <button onClick={() => props.history.push('/topics')}>Topics</button>
-      <h1> HATS PAGE </h1>
-    </div>
-  );
-};
-
-const TopicsList = props => {
-  return(
-    <div>
-      <h1> TOPIC LIST PAGE </h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-    </div>
-  );
-};
-
-const TopicDetail = (props) => {
-  return(
-    <div>
-      <h1> TOPIC DETAIL PAGE { props.match.params.topicId }</h1>
-    </div>
-  );
-};
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return <div>
     <Switch>
       <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/topics' component={TopicsList}></Route>
-      <Route path='/topics/:topicId' component={TopicDetail}></Route>
+      <Route exact path='/hats' component={HatsPage}></Route>
     </Switch>
     {/* <HomePage></HomePage> */}
   </div>;
--- ecommerce/src/components/directory/directory.component.jsx
+++ ecommerce/src/components/directory/directory.component.jsx
@@ -49,8 +49,8 @@ class Directory extends React.Component {
     render() {
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                {this.state.sections.map(({id, ...otherSectios}) => (
+                    <MenuItem key={id} {...otherSectios}></MenuItem>
                 ))}
             </div>
         )
