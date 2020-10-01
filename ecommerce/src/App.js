3fe95be0fe1609a270b87796f73507116cd33775
Osiris Román <osirisr1994@gmail.com>
2020-09-30 21:29:07 -0500

    Shop Page + Collection Preview
    Se añadió la página de tienda más un preview de los artículos disponibles en cada categoría. Muchos archivos cambiaron debido a que se instaló la extensión prettier en el editor vs code.

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,22 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
-  return <div>
-    <Switch>
-      <Route exact path='/' component={HomePage}></Route>
-      <Route exact path='/hats' component={HatsPage}></Route>
-    </Switch>
-    {/* <HomePage></HomePage> */}
-  </div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop' component={ShopPage}></Route>
+      </Switch>
+      {/* <HomePage></HomePage> */}
+    </div>
+  );
 }
 
 export default App;
