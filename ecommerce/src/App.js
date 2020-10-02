7681fb33f1fbf6dcc9b5145f504da09c7427ca80
Osiris Román <osirisr1994@gmail.com>
2020-10-01 19:28:50 -0500

    SignIn Component
    Se añadió el componente que permitirá al usuario ingresar mediante su email y su contraseña.

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
+        <Route exact path='/signin' component={SignInAndSignUp}></Route>
       </Switch>
       {/* <HomePage></HomePage> */}
     </div>
