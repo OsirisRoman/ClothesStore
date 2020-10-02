3b81b4df4d0da1520ed13580276680986abdc4c1
Osiris Román <osirisr1994@gmail.com>
2020-10-02 16:33:47 -0500

    Custom Button Component
    Se añadió un componente de un botón personalizado que se reusará en otros varios componentes.

--- ecommerce/src/components/sign-in/sign-in.component.jsx
+++ ecommerce/src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -44,7 +45,7 @@ class SignIn extends React.Component {
             value={this.state.password}
             handleChange={this.handleChange}
             required></FormInput>
-          <input type='submit' value='Submit Form'></input>
+          <CustomButton type='submit'> Sign In </CustomButton>
         </form>
       </div>
     );
