6956aafdf072607d5bb621d814832cc17ce2276f
Osiris Román <osirisr1994@gmail.com>
2020-10-03 13:46:25 -0500

    Handling User Authentication
    Se transformó el componente funcional en un componente de clase debido a que ahora se necesita que el componente maneje como estado al usuario que se está loggeando.
    Se añadieron los métodos necesarios para que la aplicación maneje persistencia de usuario y detecte cuando el usuario cierre sesión.

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -7,19 +7,43 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/shop' component={ShopPage}></Route>
-        <Route exact path='/signin' component={SignInAndSignUp}></Route>
-      </Switch>
-      {/* <HomePage></HomePage> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsuscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsuscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsuscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/shop' component={ShopPage}></Route>
+          <Route exact path='/signin' component={SignInAndSignUp}></Route>
+        </Switch>
+        {/* <HomePage></HomePage> */}
+      </div>
+    );
+  }
 }
 
 export default App;
