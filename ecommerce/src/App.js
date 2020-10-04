113f701d2cf0caed0075b7a0567bd610cf22643f
Osiris Román <osirisr1994@gmail.com>
2020-10-04 17:41:22 -0500

    Storing User Data Inside App State
    Se añadió la característica de tomar los datos del usuario que ingresa mediante inicio de sesión con Google y alojarlos como parte del estado de la aplicación para usarlos en alguna otra parte de la aplicación.

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -21,8 +21,26 @@ class App extends React.Component {
   unsuscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsuscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user);
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
