4658cd636b031e179b453800eabd6ccf03a366ed
Osiris Román <osirisr1994@gmail.com>
2020-10-04 13:50:03 -0500

    Storing users in Firebase database (Firestore)
    Se añadió un método que, toma los datos del usuario que inicia sesión en nuestra app con Google y guarda estos datos en la colección "usuarios" creada en la base de datos de firebase como objetos.

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,9 +21,8 @@ class App extends React.Component {
   unsuscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsuscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      createUserProfileDocument(user);
     });
   }
 
--- ecommerce/src/firebase/firebase.utils.js
+++ ecommerce/src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ const config = {
   appId: '1:383214394371:web:8cef481d359c277e26c236',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth}`);
+
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
