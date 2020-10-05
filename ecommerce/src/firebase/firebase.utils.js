c498d47baeceb6d7131263116c8cff12ab707728
Osiris Román <osirisr1994@gmail.com>
2020-10-05 17:33:51 -0500

    Sign Up with Email and Password
    Se añadió un componente que le permite al usuario registrarse con un email y una contraseña, además de los métodos necesarios que guardan los datos de registro del usuario en una base de datos no relacional.

--- ecommerce/src/firebase/firebase.utils.js
+++ ecommerce/src/firebase/firebase.utils.js
@@ -15,8 +15,7 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth}`);
-
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapshot = await userRef.get();
 
   if (!snapshot.exists) {
