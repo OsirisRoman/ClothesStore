9e4b3048f630008d9bf48f3e6100e5335d123bb5
Osiris Román <osirisr1994@gmail.com>
2020-10-09 17:12:40 -0500

    Hiding the login component from logged users
    Se ocultó el componente de inicio y registro de usuarios de los usuarios que ya han iniciado sesión. Además se hizo uso de un objeto que guarda el valor de las constantes comunes dentro del user reducer.

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -44,16 +44,28 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}></Route>
           <Route exact path='/shop' component={ShopPage}></Route>
-          <Route exact path='/signin' component={SignInAndSignUp}></Route>
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/'></Redirect>
+              ) : (
+                <SignInAndSignUp></SignInAndSignUp>
+              )
+            }></Route>
         </Switch>
-        {/* <HomePage></HomePage> */}
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
