ad54a212c74d9b298c66ad115c4de783c75f4dff
Osiris Román <osirisr1994@gmail.com>
2020-10-14 21:45:02 -0500

    Rewrite selector
    Se reescribió el selector usando la librería reselect para que el componente cart-items se re-renderice solo cuando sea necesario, mejorando el rendimiento de la aplicación.

---
+++ ecommerce/src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+import CartItem from '../../components/cart-item/cart-item.component';
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
