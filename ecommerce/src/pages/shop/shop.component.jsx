f4e43d02c9cae4f8afd5e2043a2603fc9d288227
Osiris Román <osirisr1994@gmail.com>
2020-10-22 21:42:24 -0500

    Moving Collections Map Function Into Its Own Component
    Se creó un componente que se encarga de mapear las categorías y mostrarlas usando el componente CollectionPreview. Esto nos servirá más adelante cuando se haga routing del shop page.

--- ecommerce/src/pages/shop/shop.component.jsx
+++ ecommerce/src/pages/shop/shop.component.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-/* import { selectCartItems } from '../../redux/cart/cart.selectors'; */
-
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 const ShopPage = ({ collections }) => (
   <div className='shop-page'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
