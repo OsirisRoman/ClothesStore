d6157a268e94521cb84550f530118424484d1611
Osiris Román <osirisr1994@gmail.com>
2020-10-25 13:41:11 -0500

    Retrieving products for a given collection.
    Se implementó la característica de obtener los productos de una determinada colleción de artículos basados en la url a la que se accede.

--- ecommerce/src/pages/collection/collection.component.jsx
+++ ecommerce/src/pages/collection/collection.component.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
   <div className='collection-page'>
     <h2>COLLECTION PAGE</h2>
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
