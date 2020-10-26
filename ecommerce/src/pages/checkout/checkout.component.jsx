353e64f9f6cf1bdf43c2bf7b7fdbaad5d5015ee6
Osiris Román <osirisr1994@gmail.com>
2020-10-25 20:34:05 -0500

    Stripe Integration
    Se añadió un componente que permite pagar con tarjeta de crédito o débito a través de Stripe pulsando el botón de pago dentro del carrito.

--- ecommerce/src/pages/checkout/checkout.component.jsx
+++ ecommerce/src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
@@ -36,6 +37,20 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/25 - CVV: 123
+      <br />
+      <span
+        className='external-link'
+        onClick={() =>
+          window.open('https://stripe.com/docs/testing#cards', '_blank')
+        }>
+        No Sirve La Tarjeta
+      </span>
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
