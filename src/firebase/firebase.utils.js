041f2bbb091c02225f67f9dc573a6016eb63d5f9
Osiris Román <osirisr1994@gmail.com>
2020-10-29 13:45:50 -0500

    Bringing the shop data from firestore to our App
    Previamente se subió el contenido del archivo shop.data ubicado en la carpeta redux a la base de datos de firebase(firestore). Estos datos se subieron de acuerdo a los tutoriales 5 y 6 de la sección 16. Ahora en este commit se agregó la característica de traer los datos de las categorías y productos de la tienda que ya se encuentran en la base de datos.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -38,6 +38,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 firebase.initializeApp(config);
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  console.log(transformedCollection);
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
